b26c8a16a3bcdd4db810ffe2fe41c7bf2e654435
gregory <greg2502@gmail.com>
2014-06-13 09:20:02 -0700

    strategy to use different storage

--- lib/jouba.rb
+++ lib/jouba.rb
@@ -1,8 +1,23 @@
+require 'virtus'
 require 'wisper'
 require 'hashie'
 require 'mongoid'
 
 Dir["#{File.dirname(__FILE__)}/jouba/**/*.rb"].each {|f| require f}
-module Jouba
 
+module Jouba
+  extend self
+
+  attr_accessor :config
+
+  def configure
+    @config = Configuration.new.tap{ |configuration| yield(configuration) }
+  end
+
+  def use(store)
+    if store == :mongo
+      Data::Event.send :include, Data::Mongoid::Event
+      Data::Snapshot.send :include, Data::Mongoid::Snapshot
+    end
+  end
 end
