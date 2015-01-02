f83ca77af1abdac37cc0d8f14a458f887484c665
gregory <greg2502@gmail.com>
2015-01-01 19:21:07 -0800

    spec the interface for a store adapter

--- lib/jouba/stores.rb
+++ lib/jouba/stores.rb
@@ -1,4 +1,27 @@
 module Jouba
   module Stores
+    def self.append_events(aggregate, events)
+      fail NotImplementedError
+    end
+
+    def self.find(id)
+      fail NotImplementedError
+    end
+
+    def self.count
+      fail NotImplementedError
+    end
+
+    def self.locked?(key)
+      fail NotImplementedError
+    end
+
+    def self.lock!(key)
+      fail NotImplementedError
+    end
+
+    def self.unlock!(key)
+      fail NotImplementedError
+    end
   end
 end
